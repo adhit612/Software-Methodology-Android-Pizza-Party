e880d81da169eae56b08eefcbb66d7d9967852fa
Abhishek Thakare <38162188+abhishekt23@users.noreply.github.com>
2023-11-30 23:45:44 -0500

    Add files via upload

---
+++ SoftwareMethodologyAndroidPP/app/src/main/java/com/softwaremethodologyandroidpp/Sauce.java
@@ -0,0 +1,29 @@
+package com.softwaremethodologyandroidpp;
+
+/**
+ * Class that manages Sauce types for pizza.
+ *
+ * @author Abhishek Thakare, Adhit Thakur.
+ */
+public enum Sauce {
+    TOMATO {
+        /**
+         * Method that handles getting the sauce of the pizza.
+         * @return sauce of pizza.
+         */
+        @Override
+        public String toString() {
+            return "Tomato";
+        }
+    },
+    ALFREDO {
+        /**
+         * Method that handles getting the sauce of the pizza.
+         * @return sauce of pizza.
+         */
+        @Override
+        public String toString() {
+            return "Alfredo";
+        }
+    },
+}
